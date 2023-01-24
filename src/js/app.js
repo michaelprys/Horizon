c7584ef8177cb15c8d0bb0990560ab6d6a78e273
Michael <musha09cz@gmail.com>
2023-01-24 01:59:33 +0100

    swiper fix

--- src/js/app.js
+++ src/js/app.js
@@ -2,7 +2,7 @@ import * as flsFunctions from "./modules/functions.js";
 flsFunctions.isWebP();
 //-----------------------------------------------------//
 import swiper from "./modules/swiper.js";
-import wedo__accordion from "./modules/accordion.js";
+import wedoAccordion from "./modules/accordion.js";
 
 // const menuBtn = document.querySelector('.navigation-toggle');
 // let menuOpen = false;
--- src/js/modules/accordion.js
+++ src/js/modules/accordion.js
@@ -1,7 +1,7 @@
-const wedo__accordion = document.getElementsByClassName("contentBx");
+const wedoAccordionHeader = document.querySelectorAll(".wedo__accordion-header");
 
-for (i = 0; i < wedo__accordion.length; i++) {
-  wedo__accordion[i].addEventListener("click", function () {
-    this.classList.toggle("active");
+wedoAccordionHeader.forEach((wedoAccordionHeader) => {
+  wedoAccordionHeader.addEventListener("click", (event) => {
+    wedoAccordionHeader.classList.toggle("active");
   });
-}
+});
